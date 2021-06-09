f197fcc7a210916f29fdc4375932b65c22e8d08a
gekiwason <ninjinx@gmail.com>
2021-06-09 21:31:08 +0900

    shopモデルとコントローラー作成

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Shop;
+
+class ShopController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $shops = Shop::all();
+        return view('shop.index', compact('shops'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('shop.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Shop::create($request->all());
+        return redirect()->route('shop.index')->with('success', '新規登録完了しました');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $shop = Shop::find($id);
+        return view('shop.show', compact('shop'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $shop = Shop::find($id);
+        return view('shop.edit', compact('shop'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $update = [
+            'name' => $request->name,
+            'access' => $request->access
+        ];
+        Shop::where('id', $id)->update($update);
+        return back()->with('success', '編集完了しました');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Shop::where('id', $id)->delete();
+        return redirect()->route('shop.index')->with('success', '削除完了しました');
+    }
+}
